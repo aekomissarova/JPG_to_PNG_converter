2f94704ceacadc4a14214470825663137e9d874f
ankomissarova <ankomissarova@ptsecurity.com>
2022-03-18 13:18:25 +0300

    delete try block

--- main.py
+++ main.py
@@ -9,17 +9,12 @@ except Exception as err:
     print(err)
     quit()
 
-#check whether output_folder doesn't exist
+# check whether output_folder doesn't exist
 if not os.path.isdir(output_folder):
     os.mkdir(output_folder)
 
 for file in os.listdir(input_folder):
-    try:
-        path = input_folder+file
+        path = input_folder + file
         image = Image.open(path)
         new_path = output_folder + os.path.splitext(file)[0] + ".png"
         image.save(new_path, "png")
-    except Exception as err:
-        print(err)
-    quit()
-
