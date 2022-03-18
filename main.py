d18bef28793d56b5b5d2293d1643431f70d92945
ankomissarova <ankomissarova@ptsecurity.com>
2022-03-18 10:04:17 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from PIL import Image
+
+try:
+    input_folder = sys.argv[1]
+    output_folder = sys.argv[2]
+except Exception as err:
+    print(err)
+    quit()
+
+#check whether output_folder doesn't exist
+if not os.path.isdir(output_folder):
+    os.mkdir(output_folder)
+
+for file in os.listdir(input_folder):
+    try:
+        path = input_folder+file
+        image = Image.open(path)
+        new_path = output_folder + os.path.splitext(file)[0] + ".png"
+        image.save(new_path, "png")
+    except Exception as err:
+        print(err)
+    quit()
+
